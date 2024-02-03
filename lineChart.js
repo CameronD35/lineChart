47081a78ac735eecd5fbfe113105664f4e135aa2
CameronD35 <camster09@ca.rr.com>
2024-02-02 21:43:28 -0800

    The chart now responds to changes in the minimum x and y values.

--- lineChart.js
+++ lineChart.js
@@ -7,6 +7,7 @@ let marginRight = 20;
 let marginBottom = 30;
 let marginLeft = 40;
 
+// Random dataset of x and y values --TESTING--
 let randomData = [
     {x: 0, y: 40},
     {x: 10, y: 50},
@@ -15,40 +16,56 @@ let randomData = [
     {x: 40, y: 90}
 ];
 
+// Current x value --TESTING--
 let globX = 40
 let globY = 90
 
-let x = d3.scaleLinear([0, 100], [marginLeft, width - marginRight]);
+// Transistion instance used for animation
+let axisTransition = d3.transition()
+.duration(500)
+.ease(d3.easeLinear)
 
-let y = d3.scaleLinear([0, 100], [height - marginBottom, marginTop]);
+// x-axis scale definition
+let x = d3.scaleLinear(d3.extent(randomData, (d) => { return d.x}), [marginLeft, width - marginRight]);
 
+// y-axis scale definition
+let y = d3.scaleLinear(d3.extent(randomData, (d) => { return d.y}), [height - marginBottom, marginTop]);
+
+// Line generator
 let line = d3.line()
 .x((d) => x(d.x))
 .y((d) => y(d.y))
 
+// Area generator
+// y0 is set to the minimum y value and y1 is set the y value at any given x value
 let area = d3.area()
 .x((d) => x(d.x))
-.y0(y(0))
-.y1((d) => y(d.y))
+.y0(y(d3.min(randomData, (e) => {return e.y})))
+.y1((d) => y(d.y));
 
+// Container for the chart
 let chartCont = document.querySelector(".chartContainer")
 
+// SVG generator
 let svg = d3.create('svg')
 .attr("width", width)
 .attr("height", height)
 .attr("color", "white")
 .attr("class", "svg");
 
-svg.append("g")
+// x-axis generator
+let xAxis = svg.append("g")
 .attr("transform", `translate(0, ${height - marginBottom})`)
 .attr("class", "xAxis")
 .call(d3.axisBottom(x));
 
-svg.append("g")
+// y-axis generator
+let yAxis = svg.append("g")
 .attr("transform", `translate(${marginLeft}, 0)`)
 .attr("class", "yAxis")
 .call(d3.axisLeft(y));
 
+// line path generator
 let lineSVG = svg.append("path")
 .datum(randomData)
 .attr("d", line)
@@ -56,6 +73,7 @@ let lineSVG = svg.append("path")
 .attr("fill", "none")
 .attr("class", "line");
 
+// area path generator
 let areaSVG = svg.append("path")
 .attr("d", area(randomData))
 .attr("stroke", "none")
@@ -63,22 +81,30 @@ let areaSVG = svg.append("path")
 .attr("fill-opacity", "0.3")
 .attr("class", "area");
 
+// circle generator
 let circle = svg.selectAll("circle")
-.data(randomData);
-
-
-circle.join("circle")
+.data(randomData)
+.join("circle")
 .attr("r", 5)
 .attr("cx", (d) => {return x(d.x)})
 .attr("cy", (d) => {return y(d.y)})
 .attr("fill", "yellow");
 
+// Append the chart to the container
 chartCont.append(svg.node());
 
-function updateLine() {
-    globX += 3;
-    globY -= 3;
+// Function for updating the graph
+function updateGraph() {
+    globX += 6;
+    globY -= 6;
     randomData.push({x: globX, y: globY});
+
+    x = d3.scaleLinear(d3.extent(randomData, (d) => { return d.x}), [marginLeft, width - marginRight]);
+
+    y = d3.scaleLinear(d3.extent(randomData, (d) => { return d.y}), [height - marginBottom, marginTop]);
+
+    xAxis.call(d3.axisBottom(x));
+    yAxis.call(d3.axisLeft(y));
 
     lineSVG
     .datum(randomData)
@@ -97,6 +123,8 @@ function updateLine() {
     .attr("cy", (d) => {return y(d.y)})
     .attr("fill", "yellow")
     .attr("class", "circle");
+
+//    d3.selectAll(".xAxis").transition(axisTransition);
 }
 
-setInterval(updateLine, 1000);
+setInterval(updateGraph, 1000);
